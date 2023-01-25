e695a369a211c81809e4bcf7bdd1575245e4b888
Svetlana Tomašik <seka.corazon@gmail.com>
2023-01-25 13:04:54 +0100

    react portals

--- src/App.js
+++ src/App.js
@@ -1,9 +1,4 @@
 import React, { useState, Fragment } from "react";
-
-// If you  directly add Fragment as a named import
-// in curly braces from "react" then you can use it in code
-// just by that name.
-
 import AddUser from "./components/Users/AddUser";
 import UsersList from "./components/Users/UsersList";
 
